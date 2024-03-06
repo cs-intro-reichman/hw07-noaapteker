f8431f987d060c333c25454ad5a036c4d569314b
Noa Apteker <noa.apteker@post.runi.ac.il>
2024-03-06 23:26:03 +0200

    :HW07

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -3,38 +3,44 @@
 public class HashTagTokenizer {
 
 	public static void main(String[] args) {
-
 		String hashTag = args[0];
 		String []dictionary = readDictionary("dictionary.txt");
+		hashTag = hashTag.toLowerCase();
 		breakHashTag(hashTag, dictionary);
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
 		// Your code here
-
+		for (int i = 0; i < dictionary.length; i++) {
+			dictionary[i] = in.readLine();
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
 		// Your code here
+		for (int i = 0; i < dictionary.length; i++) {
+			if (word.equals(dictionary[i])) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
-
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
             return;
         }
- 
         int N = hashtag.length();
-
         for (int i = 1; i <= N; i++) {
-		
-        }
+			if (existInDictionary(hashtag.substring(0,i),dictionary)) {
+				System.out.println(hashtag.substring(0,i));
+				breakHashTag(hashtag.substring(i),dictionary);
+				return;
+			}
+		}
     }
-
 }
